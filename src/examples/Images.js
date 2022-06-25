54174f0dca4f625c718b269628c4bb529585a6f0
Rosemary Ku <rosemaryku10@gmail.com>
2022-06-25 18:42:59 -0400

    edit about page

---
+++ src/examples/Images.js
@@ -0,0 +1,35 @@
+import React from "react"
+import styled from "styled-components"
+import { StaticImage } from "gatsby-plugin-image"
+
+const Images = () => {
+  return (
+    <Wrapper>
+      <article>
+        <h4>Constrained / Default </h4>
+        <StaticImage
+          src="../assets/images/recipes/recipe-1.jpeg"
+          alt="food"
+          height={400}
+          placeholder="tracedSVG"
+          layout="constrained"
+          className="example-img"
+          as="section"
+        />
+      </article>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+  width: 70vw;
+  margin: 0 auto;
+  article {
+    border: 2px solid red;
+  }
+  .example-img {
+    border-radius: 1rem;
+  }
+`
+
+export default Images
