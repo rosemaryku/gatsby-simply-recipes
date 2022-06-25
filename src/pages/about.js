1c50a3844fa1cc95affac22357b47c1713658745
Rosemary Ku <rosemaryku10@gmail.com>
2022-06-25 12:16:16 -0400

    add gatsby pages and layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
 
-export default function Home() {
+const about = () => {
   return (
     <Layout>
-      <h1>Home page</h1>
+      <h1>About page</h1>
     </Layout>
   )
 }
+
+export default about
