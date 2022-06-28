70da17f39ee0050c50b6e8ab6271219bc95e9fd3
Rosemary Ku <rosemaryku10@gmail.com>
2022-06-28 14:18:12 -0400

    create recipe template

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Simply Recipes
 
 - Based on the freeCodeCamp Gatsby Tutorial available on [Youtube](https://www.youtube.com/watch?v=RaTpreA0v7Q&ab_channel=freeCodeCamp.org) by John Smilga
-- Starter Code can be [found here](https://github.com/john-smilga/gatsby-v3-tutorial-recipes)
+- Completed Code can be [found here](https://github.com/john-smilga/gatsby-v3-tutorial-recipes)
 - Completed project is [found here](https://gatsby-v3-tutorial-recipes.netlify.app/tags)
 
 # To Start
--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,15 +1,110 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { BsClockHistory, BsClock, BsPeople } from "react-icons/bs"
+import Layout from "../components/Layout"
 
 // Props is automatically passed to each page along with the title (recipe name)
 const RecipeTemplate = ({ data }) => {
-  console.log(props)
+  // console.log(props)
+  const {
+    title,
+    cookTime,
+    content,
+    prepTime,
+    servings,
+    description: { description },
+    image,
+  } = data.contentfulRecipe
+  const pathToImage = getImage(image)
+  const { tags, instructions, ingredients, tools } = content
   return (
-    <div>
-      <h4> {props.params.title}</h4>
-    </div>
+    <Layout>
+      <main className="page">
+        <div className="recipe-page">
+          {/* hero  */}
+          <section className="section recipe-hero">
+            <GatsbyImage
+              image={pathToImage}
+              alt={title}
+              className="about-img"
+            />
+            <article className="recipe-info">
+              <h2>{title}</h2>
+              <p>{description}</p>
+              {/* icons */}
+              <div className="recipe-icons">
+                <article>
+                  <BsClock />
+                  <h5>prep time</h5>
+                  <p>{prepTime} min.</p>
+                </article>
+                <article>
+                  <BsClockHistory />
+                  <h5>prep time</h5>
+                  <p>{cookTime} min.</p>
+                </article>
+                <article>
+                  <BsPeople />
+                  <h5>servings</h5>
+                  <p>{servings}</p>
+                </article>
+              </div>
+              {/* tags */}
+              <p className="recipe-tags">
+                Tags:{" "}
+                {tags.map((tag, index) => {
+                  return (
+                    <Link to={`${tag}`} key={index}>
+                      {tag}
+                    </Link>
+                  )
+                })}
+              </p>
+            </article>
+          </section>
+          {/* rest of content */}
+          <section className="recipe-content">
+            <article className="second-column">
+              <h4>instructions</h4>
+              {instructions.map((item, index) => {
+                return (
+                  <div key={index} className="single-instruction">
+                    <header>
+                      <p>step {index + 1}</p>
+                      <div></div>
+                    </header>
+                    <p>{item}</p>
+                  </div>
+                )
+              })}
+            </article>
+            <article>
+              <div>
+                <h4>ingredients</h4>
+                {ingredients.map((item, index) => {
+                  return (
+                    <p key={index} className="single-ingredient">
+                      {item}
+                    </p>
+                  )
+                })}
+              </div>
+              <div>
+                <h4>tools</h4>
+                {tools.map((item, index) => {
+                  return (
+                    <p key={index} className="single-tool">
+                      {item}
+                    </p>
+                  )
+                })}
+              </div>
+            </article>
+          </section>
+        </div>
+      </main>
+    </Layout>
   )
 }
 
